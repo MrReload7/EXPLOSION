66eabbdbe9f0b4ff7669d27303162aa7ef18fffe
MrReload7 <anderbra010@battlegroundps.org>
2025-05-20 08:11:19 -0700

    Update readCSV.java

--- readCSV.java
+++ readCSV.java
@@ -1,17 +1,16 @@
+package src;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-
-public class readCSV {
-
+public class CSVReader {
     public static void main(String [] args){
 
-
+        String file = "worldcities.csv";
         //instantiate CSV reader class
         
-        CSVReader reader = new CSVReader(new FileReader(worldcities.csv));
+        CSVReader reader = new CSVReader(new FileReader(file));
 
     }
 }
