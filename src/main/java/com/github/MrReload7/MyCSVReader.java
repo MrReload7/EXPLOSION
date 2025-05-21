071f17e8eba3c4cd5b30e84039603a680c90954e
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-05-21 17:54:16 +0000

    Handles file not found
    If you look at the method signature for `new FileReader` you'll see that it can throw a FileNotFoundException.
    
    This needs to be handled in some way, otherwise the compiler will correctly error out, because you might be passing a FileReader that wasn't successfully created into the CSVReader.

--- src/main/java/com/github/MrReload7/MyCSVReader.java
+++ src/main/java/com/github/MrReload7/MyCSVReader.java
@@ -2,6 +2,8 @@ package com.github.MrReload7;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -10,9 +12,16 @@ public class MyCSVReader {
     public static void main(String [] args){
 
         String file = "worldcities.csv";
-        //instantiate CSV reader class
+        FileReader fr;
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to load file");
+            System.exit(1);
+            return;
+        }
         
-        CSVReader reader = new CSVReader(new FileReader(file));
+        CSVReader reader = new CSVReader(fr);
 
     }
 }
