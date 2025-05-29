9a207a36aa2b21a4f6de912dd472d3fb8a3dd70f
MrReload7 <anderbra010@battlegroundps.org>
2025-05-29 15:28:04 +0000

    Mathamatical corrections

--- src/main/java/com/github/MrReload7/nukeCalc.java
+++ src/main/java/com/github/MrReload7/nukeCalc.java
@@ -4,7 +4,9 @@ public class nukeCalc {
     public nukeCalc(){}
 
     public double getCasualties(int yeild, double density){
-        double area = Math.cbrt(yeild)*0.28;
+        double pi = Math.PI;
+        double area = pi*Math.pow((Math.cbrt(yeild)*0.28), 2);
+        
         return area * density;
     }
 
